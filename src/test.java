3e48ce2b984a23ee04f3caa06628441671acd83a
Ghassem <ghassem+github@gmail.com>
2014-02-06 05:39:15 -0500

    Sort Args passed by command line

--- src/test.java
+++ src/test.java
@@ -1,9 +1,16 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 /**
  * Created by ghassem on 2/6/2014.
  */
 public class test {
 
     public static void main(String[] Args) {
-        System.out.println("Hi there");
+        System.out.println(Args.length);
+        Arrays.sort(Args);
+        for (String arg: Args)
+            System.out.print (arg + " ");
+        System.out.println();
     }
 }
